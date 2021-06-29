6a393c92a9a693c0c1966fba1c6cf731eb6cbe9a
Lim Hui Jie <huijielim@gmail.com>
2021-06-29 12:51:40 +0800

    change number reference in array

--- M2-Arrays/mainsolution.js
+++ M2-Arrays/mainsolution.js
@@ -2,7 +2,7 @@
 
 const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let elementAtIndexFive = numArray[5];
+let elementAtIndexFive = numArray[6];
 
 //open the browser console to check results
 console.log('The array element with index 5 is: ', elementAtIndexFive);
